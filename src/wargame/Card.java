d55beb46d1dda3c7b985d8b2d74f12d5d8e7d403
Juan Aranda <>
2020-07-03 20:58:27 -0400

    Updated Card Class

---
+++ src/wargame/Card.java
@@ -0,0 +1,59 @@
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ * @author Juan Aranda
+ */
+package wargame;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
+ * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ *
+ * @author dancye
+ */
+public abstract class Card {
+    
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value; //1-13
+   
+    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+   
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
